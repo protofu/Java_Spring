431a9c06a10d204c5d620570337b364fd7dbb625
jehyun Sung <118592515+protofu@users.noreply.github.com>
2024-08-12 11:18:32 +0900

    feat : controller & service

---
+++ board/src/main/java/com/board/controller/BoardController.java
@@ -0,0 +1,22 @@
+package com.board.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.board.service.BoardService;
+
+@Controller // 컨트롤러 지정 어노테이션
+public class BoardController {
+	@Autowired
+	private BoardService bs;
+	
+	@RequestMapping("/board/list") // 요청에 맞는 주소 지정
+	public ModelAndView boardList() {
+		// src/main/resource/templates/board/list
+		ModelAndView mv = new ModelAndView("board/list");
+		// 비지니스 로직 수행
+		return mv;
+	}
+}
