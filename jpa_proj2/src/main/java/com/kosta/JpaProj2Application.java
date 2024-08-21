e7edb288ee9d6e7921a8173fc1b408f6721e2a7b
jehyun Sung <118592515+protofu@users.noreply.github.com>
2024-08-21 10:53:50 +0900

    init : jpa-proj2

---
+++ jpa_proj2/src/main/java/com/kosta/JpaProj2Application.java
@@ -0,0 +1,13 @@
+package com.kosta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaProj2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaProj2Application.class, args);
+	}
+
+}
